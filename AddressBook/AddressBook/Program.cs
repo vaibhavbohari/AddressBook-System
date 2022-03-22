5162c7e6440a0b38bc350c3580d1dd5bc6d7f069
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-22 20:05:26 +0530

    Adress Book

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Address Book Program");
+﻿using System;
+
+namespace AddressBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname;
+            string lastname;
+            string address;
+            string city;
+            string state;
+            string zipcode;
+            string email;
+            string phoneNumber;
+
+            Console.WriteLine("Enter the FirstName : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the lastName : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the address : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the city : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the state : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the zipcode : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the email : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the phoneNumber : ");
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
